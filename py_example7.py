9cadef55dfa4d620f00c498c6757adaa66e63684
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-11-04 14:48:18 +0300

    Add files via upload

---
+++ py_example7.py
@@ -0,0 +1,18 @@
+#Rümeysa Coşkun
+#Python değişken isimleri örnekleri.
+
+#uygun olan hata vermeyen değişken isimlendrime şekilleri:
+myvar = "x"
+my_var = "x"
+_my_var = "x"
+myVar = "x"
+myVAR = "x"
+myvar2 = "x"
+
+#uygun olmayan hata veren değişken isimlendirme şekilleri(kullanılmayan)
+
+# 2myvar = "x"
+# my-var = "x"
+# my var = "x"
+
+
